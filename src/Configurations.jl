ff5ddaf3fad437a35039bff6190702c2dbd3da6b
Elke Pahl <Elke.Pahl@auckland.ac.nz>
2021-10-01 19:33:48 +1300

    new Configuration type

--- src/Configurations.jl
+++ src/Configurations.jl
@@ -1,8 +1,22 @@
-module Configurations
-# structs for configurations
+"""
+    module Configurations
 
-export Config, Point
-export dist2
+This module defines types and functions for working with atomic configurations of N atoms.
+
+## Exported types  
+- [`Point`](@ref) 
+- [`Config`](@ref) 
+
+## Exported functions
+- [`dist2`](@ref)
+- [`move_atom!`](@ref)
+"""
+module Configurations
+
+export Config, Config1, Point
+export dist2, move_atom!
+
+using StaticArrays
 
 using ..BoundaryConditions
 
@@ -12,18 +26,28 @@ Generates a point with x,y, and z coordinate.
 
 See [`dist2`](@ref).
 """
-struct Point{T}
+struct Point{T} #<: AbstractVector 
     x::T
     y::T
     z::T
 end
+
+# Base.size(::Point) = Tuple(3)
+# Base.IndexStyle(::Point}) = IndexLinear()
+# Base.getindex(::Point, i::Int) = 
+
+
+import Base: +
+
+(+)(p1::Point, p2::Point) = Point(p1.x+p2.x, p1.y+p2.y, p1.z+p2.z)
+(+)(p1::Point, v::Union{Tuple,AbstractVector}) = Point(p1.x+v[1], p1.y+v[2], p1.z+v[3])
 
 """
     dist2(p1::Point,p2::Point) 
     
 Computes squared distance between two [`Point`](@ref)s.
 """
-dist2(p1::Point,p2::Point) = ((p1.x - p2.x)^2 + (p1.y - p2.y)^2 +(p1.z - p2.z)^2)
+dist2(p1::Point, p2::Point) = ((p1.x - p2.x)^2 + (p1.y - p2.y)^2 +(p1.z - p2.z)^2)
 
 # struct for configurations
 """
@@ -37,6 +61,7 @@ struct Config{N, BC, T}
 end
 
 #type constructors - not type stable! (as N can only be determined during run time when positions known)
+#one can check with @code_warntype
 function Config(positions::Vector{Point{T}}, boundary::BC) where {T,BC<:AbstractBC}
     N = length(positions)
     return Config{N,BC,T}(positions,boundary)
@@ -51,5 +76,29 @@ end
 #overloads Base function length
 Base.length(::Config{N}) where N = N
 
+struct Config1{N, BC, T} 
+    pos::Vector{SVector{3,T}}
+    bc::BC
+end
+
+function Config1(pos::Vector{SVector{3,T}}, bc::BC) where {T,BC<:AbstractBC}
+    N = length(pos)
+    return Config1{N,BC,T}(pos,bc)
+end
+
+#type stable constructor as N is passed along (for compiler)
+function Config1{N}(pos::Vector{SVector{3,T}}, bc::BC) where {N,T,BC<:AbstractBC}
+    @boundscheck length(pos) == N || error("number of atoms and number of positions not the same")
+    return Config1{N,BC,T}(pos,bc)
+end
+
+"""
+    move_atom!(config::Config, n_atom, delta_move)
+
+Moves n_atom-th atom in configuration by delta_move  
+"""
+function move_atom!(config::Config, n_atom, delta_move)
+    config.points[n_atom] += delta_move
+end
 
 end
