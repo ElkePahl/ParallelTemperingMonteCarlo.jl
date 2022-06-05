cd14e59aed3f5e727b9918d012f882af536cb9aa
Elke Pahl <Elke.Pahl@auckland.ac.nz>
2022-06-05 17:33:48 +1200

    added to input script

--- scripts/run_ne13.jl
+++ scripts/run_ne13.jl
@@ -1,13 +1,21 @@
 using ParallelTemperingMonteCarlo
 
+#temperature grid
 ti = 2.
 tf = 40.
 n_traj = 30
 
+#Monte Carlo Cycles
 mc_cycles = 10000
+mc_sample = 1
 
+#define boundary conditions 
+bc_ne13 = SphericalBC(radius=5.32)   #Angstrom
+
+#starting configuration
+conf_ne13 = Config(pos_ne13, bc_ne13)
+# max. displacement
 max_displ = 0.1 # Angstrom
-
 
 temp = TempGrid{n_traj}(ti,tf) # move to input file at a later stage ...
 
