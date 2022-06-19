4c5e5c797cc0c37c353d04a39ef71fbecabacba6
Elke Pahl <Elke.Pahl@auckland.ac.nz>
2022-06-19 13:43:56 +1200

    +displ in MCStates

--- src/MCRun.jl
+++ src/MCRun.jl
@@ -17,22 +17,23 @@ struct MCState{T,N,BC,M}
     config::Config{N,BC,T}
     dist2_mat::Matrix{T}
     en_atom_mat::Vector{T}
-    en_tot::ref{T}
+    en_tot::Ref{T}
     ham::Vector{T}
+    max_displ::SVector{3,Int}
     count_atom::SVector{2,Int}
     count_vol::SVector{2,Int}
     count_rot::SVector{2,Int}
     count_exc::SVector{2,Int}
 end    
 
-function MCState(temp, beta, config::Config{N,BC,T}, dist2_mat, en_atom_mat, en_tot, ham; count_atom=SVector(0,0), count_vol=SVector(0,0), count_rot=SVector(0,0), count_exc=SVector(0,0)) where {T,N,BC,M}
-    MCState{T,N,BC,M}(temp,beta,config,dist2_mat,en_atom_mat,en_tot,ham,count_atom,count_vol,count_rot,count_exc)
+function MCState(temp, beta, config::Config{N,BC,T}, dist2_mat, en_atom_mat, en_tot, ham; max_displ=SVector(0.1,0.1,1.) ,count_atom=SVector(0,0), count_vol=SVector(0,0), count_rot=SVector(0,0), count_exc=SVector(0,0)) where {T,N,BC,M}
+    MCState{T,N,BC,M}(temp,beta,config,dist2_mat,en_atom_mat,en_tot,ham,max_displ,count_atom,count_vol,count_rot,count_exc)
 end
 
-function MCState(temp, beta, config::Config{N,BC,T}, ham; count_atom=SVector(0,0), count_vol=SVector(0,0), count_rot=SVector(0,0), count_exc=SVector(0,0)) where {T,N,BC,M}
+function MCState(temp, beta, config::Config{N,BC,T}, ham, max_displ; count_atom=SVector(0,0), count_vol=SVector(0,0), count_rot=SVector(0,0), count_exc=SVector(0,0)) where {T,N,BC,M}
     dist2_mat = get_distance2_mat(config)
     en_atom_mat, en_tot = dimer_energy_config(dist2_mat_0, n_atoms, pot)
-    MCState{T,N,BC,M}(temp,beta,config,dist2_mat,en_atom_mat,en_tot,ham,count_atom,count_vol,count_rot,count_exc)
+    MCState{T,N,BC,M}(temp,beta,config,dist2_mat,en_atom_mat,en_tot,ham,max_displ,count_atom,count_vol,count_rot,count_exc)
 end
 
 """
@@ -74,9 +75,9 @@ function mc_step_atom!(config, beta, dist2_mat, en_tot, i_atom, max_displacement
     return config, entot, dist2mat, count_acc, count_acc_adjust
 end
 
-function mc_step!(::AtomMove, i_move, mc_state::MCState, i_atom, pot, ensemble)
+function atom_move!(mc_state::MCState, i_atom, pot, ensemble)
     #move randomly selected atom (obeying the boundary conditions)
-    trial_pos = atom_displacement(mc_state.config.pos[i_atom], mc_state.moves[i_move].max_displacement, mc_state.config.bc)
+    trial_pos = atom_displacement(mc_state.config.pos[i_atom], mc_state.max_displacement[1], mc_state.config.bc)
     #find new distances of moved atom - might not be always needed?
     delta_en, dist2_new = energy_update(trial_pos, i_atom, mc_state.config, mc_state.dist2_mat, pot)
     #dist2_new = [distance2(trial_pos,b) for b in config.pos]
@@ -91,42 +92,55 @@ function mc_step!(::AtomMove, i_move, mc_state::MCState, i_atom, pot, ensemble)
         mc_state.dist2_mat[i_atom,:] = copy(dist2_new)
         mc_state.dist2_mat[:,i_atom] = copy(dist2_new)
         mc_state.en_tot[] += delta_en
-        mc_state.moves[i_move].count_acc += 1
-        mc_state.moves[i_move].count_acc_adj += 1
+        c = mc_state.count_atom
+        mc_state.count_atom = SVector(c[1]+1,c[2]+1)
     end 
     return mc_state #config, entot, dist2mat, count_acc, count_acc_adjust
 end
 
-function mc_cycle!(n_moves, type_moves, mc_states, mc_params, pot, ensemble)
+function mc_step!(mc_state::MCState, move_strat, pot, ensemble)
+    a = atom_move_frequency(move_strat)
+    v = vol_move_frequency(move_strat)
+    r = rot_move_frequency(move_strat)
+
+    ran = rand(1:(a+v+r)) #choose move randomly
+    if ran <= a
+        mc_state = atom_move!(mc_state, ran, pot, ensemble)
+    #else if ran <= v
+    #    mc_state = vol_move!(mc_state, pot, ensemble)
+    #else if ran <= r
+    #    mc_state = rot_move!(mc_state, pot, ensemble)
+    end
+    return mc_state
+end 
+
+
+function mc_cycle!(mc_states, move_strat, mc_params, pot, ensemble)
     for i_traj=1:mc_params.n_traj
         for i_move=1:n_moves
-            ran = rand(1:n_moves) #choose move randomly
-            #println(ran, " ", i_traj, " ", mc_states[i_traj].moves)
-            mc_states[i_traj] = mc_step!(type_moves[ran][2], type_moves[ran][1], mc_states[i_traj], ran, pot, ensemble)
-            #for i_traj=1:mc_params.n_traj
-            #    println(mc_states[i_traj].moves[1].count_acc)
-            #end
+            #mc_states[i_traj] = mc_step!(type_moves[ran][2], type_moves[ran][1], mc_states[i_traj], ran, pot, ensemble)
+            mc_states[i_traj] = mc_step!(mc_states[i_traj], move_strat, pot, ensemble)
         end
         push!(mc_states[i_traj].ham, mc_states[i_traj].en_tot[]) #to build up ham vector of sampled energies
     end
     return mc_states
 end
 
-function ptmc_run!(mc_states, mc_params, pot, ensemble, n_bin)
+function ptmc_run!(mc_states, move_strat, mc_params, pot, ensemble, n_bin)
     
     #number of moves per MC cycle
-    moves = mc_states[1].moves
-    n_moves = 0
-    type_moves = []
-    for i in eachindex(moves)
-        n_moves += moves[i].frequency
-        for j=1:moves[i].frequency
-            push!(type_moves,(i,moves[i]))
-        end 
-    end
+    #moves = mc_states[1].moves
+    #n_moves = 0
+    #type_moves = []
+    #for i in eachindex(moves)
+    #    n_moves += moves[i].frequency
+    #    for j=1:moves[i].frequency
+    #        push!(type_moves,(i,moves[i]))
+    #    end 
+    #end
     
     for i=1:mc_params.mc_cycles
-        mc_states = mc_cycle!(n_moves, type_moves, mc_states, mc_params, pot, ensemble)
+        mc_states = mc_cycle!(mc_states, move_strat, mc_params, pot, ensemble)
     end 
     #TO DO
     # atom, volume moves ...
