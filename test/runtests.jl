8397778a7d4614ee3f1161eadd84d654cd7bcfb2
Elke Pahl <Elke.Pahl@auckland.ac.nz>
2022-07-06 16:18:58 +1200

    added test set EnergyEvaluation

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,6 @@ using Test
 using ParallelTemperingMonteCarlo
 using StaticArrays, LinearAlgebra
 
-# @testset "Point" begin
-#     p1 = Point(1.,1.,1.)
-#     @test p1.x == p1.y == p1.z == 1.
-
-#     pointarray = [Point(rand(),rand(),rand()) for _ in 1:10]
-#     bc = SphericalBC(1.0)    
-#     config = @inferred Config{10}(pointarray,bc)
-#     @test length(config) == 10
-#     @test_throws ErrorException @inferred Config(pointarray,bc)
-#     p2 = Point(-1.,-1.,-1.)
-#     @test dist2(p1,p2) == 12.0
-# end
-
 @testset "Config" begin
     bc = SphericalBC(radius=2.0)
     v1 = SVector(1., 2., 3.)
@@ -53,7 +40,6 @@ end
 @testset "BoundaryConditions" begin
     bc = SphericalBC(radius=1.0)
     @test bc.radius2 == 1.
-
     @test check_boundary(bc,SVector(0,0.5,1.))
     @test check_boundary(bc,SVector(0,0.5,0.5)) == false
 end
@@ -83,3 +69,49 @@ end
     @test en_vec[2] == dimer_energy_atom(2,d2mat[2,:],pot)
     #@test en_vec
 end
+
+@testset "EnergyEvaluation"
+    pos_ne13 = [[2.825384495892464, 0.928562467914040, 0.505520149314310],
+                [2.023342172678102,	-2.136126268595355, 0.666071287554958],
+                [2.033761811732818,	-0.643989413759464, -2.133000349161121],
+                [0.979777205108572,	2.312002562803556, -1.671909307631893],
+                [0.962914279874254,	-0.102326586625353, 2.857083360096907],
+                [0.317957619634043,	2.646768968413408, 1.412132053672896],
+                [-2.825388342924982, -0.928563755928189, -0.505520471387560],
+                [-0.317955944853142, -2.646769840660271, -1.412131825293682],
+                [-0.979776174195320, -2.312003751825495, 1.671909138648006],
+                [-0.962916072888105, 0.102326392265998,	-2.857083272537599],
+                [-2.023340541398004, 2.136128558801072,	-0.666071089291685],
+                [-2.033762834001679, 0.643989905095452, 2.132999911364582],
+                [0.000002325340981,	0.000000762100600, 0.000000414930733]]
+    AtoBohr = 1.88973
+    pos_ne13 = pos_ne13 * AtoBohr
+    bc_ne13 = SphericalBC(radius=5.32*AtoBohr)
+    config_ne13 = Config(pos_ne13, bc_ne13)
+    c=[-10.5097942564988, 989.725135614556, -101383.865938807, 3918846.12841668, -56234083.4334278, 288738837.441765]
+    pot = ELJPotentialEven{6}(c)
+    dist2_mat = get_distance2_mat(config_ne13)
+    @test dist2_mat[1,1] == 0.0
+    @test dist2_mat[1,2] > 35.92 && dist2_mat[1,2] < 35.93
+    @test length(config_ne13.pos) == 13
+    en_atom_vec, en_tot = dimer_energy_config(dist2_mat, 13, pot)
+    @test en_atom_vec[1] < -0.00081 && en_atom_vec[1] > -0.00082
+    @test en_tot < -0.00563 && en_tot > -0.005634686479115
+end
+
+#@testset "MCLoop"
+
+#end
+
+# @testset "Point" begin
+#     p1 = Point(1.,1.,1.)
+#     @test p1.x == p1.y == p1.z == 1.
+
+#     pointarray = [Point(rand(),rand(),rand()) for _ in 1:10]
+#     bc = SphericalBC(1.0)    
+#     config = @inferred Config{10}(pointarray,bc)
+#     @test length(config) == 10
+#     @test_throws ErrorException @inferred Config(pointarray,bc)
+#     p2 = Point(-1.,-1.,-1.)
+#     @test dist2(p1,p2) == 12.0
+# end
