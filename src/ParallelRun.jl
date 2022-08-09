8dd969a4f1e152203eccd6492b8e1b6313acfd77
Grayseff <ghun245@aucklanduni.ac.nz>
2022-08-09 12:34:44 +1200

    Created module and included dependencies

---
+++ src/ParallelRun.jl
@@ -0,0 +1,16 @@
+module ParallelRun
+
+using ..BoundaryConditions
+using ..Configurations
+using ..InputParams
+using ..MCMoves
+using ..EnergyEvaluation
+using ..MCRun
+#using ..RuNNer #this is essential for after the pull request
+
+using StaticArrays
+
+
+
+
+end
