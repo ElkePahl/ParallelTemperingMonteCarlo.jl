738f8bd5c0b9563c051e86fef53430e6fd0e3c3d
Grayseff <ghun245@aucklanduni.ac.nz>
2022-08-16 15:02:49 +1200

    moderate changes and a notebook included for tests

--- src/ParallelRun.jl
+++ src/ParallelRun.jl
@@ -1,79 +1,85 @@
 module ParallelRun
+using Threads
+using Distributed
+addprocs(nthreads())
 
-using ..BoundaryConditions
-using ..Configurations
-using ..InputParams
-using ..MCMoves
-using ..EnergyEvaluation
-using ..MCRun
+@everywhere begin
+    using ..BoundaryConditions
+    using ..Configurations
+    using ..InputParams
+    using ..MCMoves
+    using ..EnergyEvaluation
+    using ..MCRun
+    using SharedArrays
 #using ..RuNNer #this is essential for after the pull request
+end
 
 using StaticArrays
-"""
-    store_config(mc_states,mc_params)
-A function that finds the lowest energy configuration in a series of MC_State configs and returns it. 
-"""
-function store_state(mc_states,mc_params)
-    en_vec = []
-    for idx = 1:mc_params.n_traj
-        push!(en_vec, mc_states[idx].en_tot)
-    end
-    idx = whichmin(en_vec)
-    copy_state = deepcopy(mc_states[idx].config) 
+# """
+#     store_config(mc_states,mc_params)
+# A function that finds the lowest energy configuration in a series of MC_State configs and returns it. 
+# """
+# function store_state(mc_states,mc_params)
+#     en_vec = []
+#     for idx = 1:mc_params.n_traj
+#         push!(en_vec, mc_states[idx].en_tot)
+#     end
+#     idx = whichmin(en_vec)
+#     copy_state = deepcopy(mc_states[idx].config) 
 
-    return copy_state
-end
-
-
-function ptmc_initialise(mc_states, move_strat, mc_params, pot, ensemble,n_bin; num_threads = Threads.nthreads() )
-    #initialise the move frequency
-    a = atom_move_frequency(move_strat)
-    v = vol_move_frequency(move_strat)
-    r = rot_move_frequency(move_strat)
-
-    #steps per mc_cycle
-
-    n_steps = a + v + r
-
-    println("Total number of moves per MC cycle: ", n_steps)
-    println()
+#     return copy_state
+# end
 
 
-    #set steps to equilibrate and determine the starting configs
-    steps = Int(round( mc_params.eq_cycles/num_threads ))
+# function ptmc_initialise(mc_states, move_strat, mc_params, pot, ensemble,n_bin; num_threads = Threads.nthreads() )
+#     #initialise the move frequency
+#     a = atom_move_frequency(move_strat)
+#     v = vol_move_frequency(move_strat)
+#     r = rot_move_frequency(move_strat)
 
-    state_vec = []
-    results_vec = []
-    config_vec = []
+#     #steps per mc_cycle
 
-    for j = 1:num_threads
-        for i = 1:steps
-            #begin cycles
-            @inbounds mc_states = mc_cycle!(mc_states, move_strat, mc_params, pot, ensemble, n_steps, a, v, r)
-            #adjust stepsize
-            if rem(i, mc_params.n_adjust) == 0
-                for i_traj = 1:mc_params.n_traj
-                update_max_stepsize!(mc_states[i_traj], mc_params.n_adjust, a, v, r)
-                end 
-            end
-        end
-        #store the lowest energy config
-        push!(config_vec,store_config(mc_states,mc_params))
-    end
-    # we now want to initialise n_thread mc_state vectors
-    for idx = 1:num_threads
+#     n_steps = a + v + r
 
-        states = [MCState(mc_states[i].temp, mc_states[i].beta, config_vec[idx], pot; max_displ=mc_states[i].max_displ ) for i in 1:n_traj]
-        push!(state_vec,states)
+#     println("Total number of moves per MC cycle: ", n_steps)
+#     println()
 
-        result = Output{Float64}(n_bin)
-        push!(results_vec,result)
 
-    end
+#     #set steps to equilibrate and determine the starting configs
+#     steps = Int(round( mc_params.eq_cycles/num_threads ))
+
+#     state_vec = []
+#     results_vec = []
+#     config_vec = []
+
+#     for j = 1:num_threads
+#         for i = 1:steps
+#             #begin cycles
+#             @inbounds mc_states = mc_cycle!(mc_states, move_strat, mc_params, pot, ensemble, n_steps, a, v, r)
+#             #adjust stepsize
+#             if rem(i, mc_params.n_adjust) == 0
+#                 for i_traj = 1:mc_params.n_traj
+#                 update_max_stepsize!(mc_states[i_traj], mc_params.n_adjust, a, v, r)
+#                 end 
+#             end
+#         end
+#         #store the lowest energy config
+#         push!(config_vec,store_config(mc_states,mc_params))
+#     end
+#     # we now want to initialise n_thread mc_state vectors
+#     for idx = 1:num_threads
+
+#         states = [MCState(mc_states[i].temp, mc_states[i].beta, config_vec[idx], pot; max_displ=mc_states[i].max_displ ) for i in 1:n_traj]
+#         push!(state_vec,states)
+
+#         result = Output{Float64}(n_bin)
+#         push!(results_vec,result)
+
+#     end
     
-    return state_vec, results_vec
+#     return state_vec, results_vec
 
-end
+# end
 
 
 
