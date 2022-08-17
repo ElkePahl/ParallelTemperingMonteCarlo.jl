c49d9b923e970903613ae4608d76dcc7b2680580
Grayseff <ghun245@aucklanduni.ac.nz>
2022-08-17 13:39:06 +1200

    Beginning parallelisation

--- src/ParallelRun.jl
+++ src/ParallelRun.jl
@@ -82,5 +82,66 @@ using StaticArrays
 # end
 
 
+@everywhere function ptmc_run!(mc_states, move_strat, mc_params, pot, ensemble, results,parallel_run::Bool; save_ham::Bool = true, )
+    if save_ham == false
+        #If we do not save the hamiltonian we still need to store the E, E**2 terms at each cycle
+        for i_traj = 1:mc_params.n_traj
+            push!(mc_states[i_traj].ham, 0)
+            push!(mc_states[i_traj].ham, 0)
+        end
+    end
+
+    a = atom_move_frequency(move_strat)
+    v = vol_move_frequency(move_strat)
+    r = rot_move_frequency(move_strat)
+    #number of MC steps per MC cycle
+    n_steps = a + v + r
+
+    println("Total number of moves per MC cycle: ", n_steps)
+    println()
+
+    #equilibration cycle
+    @sync begin
+        @threads for i = 1:mc_params.eq_cycles
+            @inbounds mc_states = mc_cycle!(mc_states, move_strat, mc_params, pot, ensemble, n_steps, a, v, r)
+            if rem(i, mc_params.n_adjust) == 0
+                for i_traj = 1:mc_params.n_traj
+                    @async update_max_stepsize!(mc_states[i_traj], mc_params.n_adjust, a, v, r)
+                end 
+            end
+        end
+
+    end
+
+    for i_traj = 1:mc_params.n_traj
+        mc_states[i_traj].count_atom = [0, 0]
+        mc_states[i_traj].count_vol = [0, 0]
+        mc_states[i_traj].count_rot = [0, 0]
+        mc_states[i_traj].count_exc = [0, 0]
+    end 
+    println("equilibration done")
+
+
+    @threads for i = 1:mc_params.mc_cycles
+        @inbounds mc_states = mc_cycle!(mc_states, move_strat, mc_params, pot, ensemble, n_steps, a, v, r) 
+        #sampling step
+        MCRun.sampling_step!(mc_params,mc_states,i,save_ham)
+        # if rem(i, mc_params.mc_sample) == 0
+        #     for i_traj=1:mc_params.n_traj
+        #         push!(mc_states[i_traj].ham, mc_states[i_traj].en_tot) #to build up ham vector of sampled energies
+        #     end
+        # end 
+        #step adjustment
+        if rem(i, mc_params.n_adjust) == 0
+            for i_traj = 1:mc_params.n_traj
+                update_max_stepsize!(mc_states[i_traj], mc_params.n_adjust, a, v, r)
+            end 
+        end
+    end
+
+    println("MC loop done.")
+
+
+
 
 end
