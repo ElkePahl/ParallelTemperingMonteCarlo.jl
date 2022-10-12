f7afc90f905a027afa38ac17bebd0f1f67ded77a
Grayseff <ghun245@aucklanduni.ac.nz>
2022-10-12 13:25:05 +1300

    Changes to adjacency BC

--- src/BoundaryConditions.jl
+++ src/BoundaryConditions.jl
@@ -38,10 +38,15 @@ struct PeriodicBC{T} <: AbstractBC{T}
     box_length::T
 end
 
-struct AdjacencyBC{T} <: AbstractBC{T}
+mutable struct AdjacencyBC{T} <: AbstractBC{T}
     r2_cut::T
+    adj_mat::Matrix{T}
 end
+function AdjacencyBC(r2_cut, config::Config)
+    adj_mat = find_adjmat(config,r2_cut)
 
+    AdjacencyBC(r2_cut,adj_mat)
+end
 """
     check_boundary(bc::SpericalBC,pos)
 
@@ -56,30 +61,34 @@ check_boundary(bc::SphericalBC,pos) = sum(x->x^2,pos) > bc.radius2
 Tests if whole cluster lies in the binding sphere     
 """
 test_cluster_inside(conf,bc) = sum(x->outside_of_boundary(bc,x),conf.pos) == 0
-
+"""
+    find_adjmat(dist2_matrix, r2_cut)
+    find_adjmat(config::Config{N},r2_cut)
+creates an Adjacency Matrix from either a matrix of distance squared or from a configuration. Both require a fieldname r2_cut, the square distace of the bonding radius
+"""
 function find_adjmat(dist2_matrix, r2_cut)
     adjmat = map(dist2_matrix -> ifelse(dist2_matrix <= r2_cut, 1, 0), dist2_matrix )
     
     return adjmat
 end
+find_adjmat(config::Config{N},r2_cut) where N = [ifelse(distance2(a,b)<=r2_cut,1,0) for a in config.pos, b in config.pos]
 
+# function check_boundary(bc::AdjacencyBC,dist2_matrix)
 
-function check_boundary(bc::AdjacencyBC,dist2_matrix)
+#     bcflag = false
 
-    bcflag = false
+#     for col in eachcol(find_adjmat(dist2_matrix, bc.r2_cut))
 
-    for col in eachcol(find_adjmat(dist2_matrix, bc.r2_cut))
-
-      dummysum = sum(col)
+#       dummysum = sum(col)
       
-        if dummysum < 4
-            bcflag = true
-             # break
-        end
-        #push!(SumVec, dummysum)
-    end
-    return bcflag
-end
+#         if dummysum < 4
+#             bcflag = true
+#              # break
+#         end
+#         #push!(SumVec, dummysum)
+#     end
+#     return bcflag
+# end
 
 
 end
