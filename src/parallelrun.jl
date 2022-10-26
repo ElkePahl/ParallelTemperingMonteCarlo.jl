91970f51fbde7ff5df51246587cd14c004c84d03
Grayseff <ghun245@aucklanduni.ac.nz>
2022-10-26 15:27:45 +1300

    looking into scope

--- src/parallelrun.jl
+++ src/parallelrun.jl
@@ -115,9 +115,9 @@ function parallel_equilibration(mc_states,move_strat,mc_params,pot,ensemble,resu
 
     end
 
-    for i_thread = 1:n_threads  
+    #for i_thread = 1:n_threads  
         
-        Threads.@threads for j_thread = 1:i_thread #introducing equilibration to all threads       
+        Threads.@threads for j_thread = 1:n_threads #introducing equilibration to all threads       
             thermalise!(parallel_states[j_thread],move_strat,mc_params,pot_vector[j_thread],ensemble, ebounds, n_steps, a, v, r,sample_index)
         end
          
