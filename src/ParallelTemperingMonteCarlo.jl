7416c6b517e542f07878f98f70bdd3e576268a4f
Grayseff <ghun245@aucklanduni.ac.nz>
2022-11-28 12:12:42 +1300

    reinstated mcrun etc for testing

--- src/ParallelTemperingMonteCarlo.jl
+++ src/ParallelTemperingMonteCarlo.jl
@@ -11,11 +11,11 @@ include("MCStates.jl")
 include("InputParams.jl")
 #include("Initialization")
 include("MCMoves.jl")
-#include("MCRun.jl")
+include("MCRun.jl")
 include("multihist.jl")
 #include("ReadSave.jl")
 
-#include("parallelrun.jl")
+include("parallelrun.jl")
 
 
  @reexport using .BoundaryConditions
@@ -24,14 +24,14 @@ include("multihist.jl")
  @reexport using .EnergyEvaluation
  @reexport using .MCStates
  @reexport using .InputParams
- #@reexport using .Initialization
+ @reexport using .Initialization
  @reexport using .MCMoves
- #@reexport using .MCRun
+ @reexport using .MCRun
  @reexport using .Multihistogram
  @reexport using .RuNNer
  #@reexport using .ReadSave
 
- #@reexport using .ParallelRun
+ @reexport using .ParallelRun
 
 
 
