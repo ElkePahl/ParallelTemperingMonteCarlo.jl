ff2ce32ccc11001ed7da80265fa8e0c8a990fc5b
grayseff <ghun245@aucklanduni.ac.nz>
2023-02-20 14:03:14 +0100

    large changes to sampling

--- src/Sampling.jl
+++ src/Sampling.jl
@@ -55,18 +55,13 @@ function initialise_histograms!(mc_params,results,e_bounds,bc::SphericalBC)
     end
     return delta_en_hist,delta_r2
 end
-"""
-    update_one_histval!(histogram,index)
-To be used in conjunction with the find index function above to correctly update the overall results. Mostly a wrapper function to allow correct broascasting, this then can be used on en_histogram vector of vectors. See the update_histogram! function defined below.
-"""
-function update_one_histval!(histogram,index)
-    histogram[index] += 1
-end
-function update_histograms!(mc_states,results,delta_en_hist)
-    indices = find_hist_index.(mc_states,Ref(results),Ref(delta_en_hist))
-    broadcast(update_one_histval!,results.en_histogram,indices)
 
-    return results
+function update_histograms!(mc_states,results,delta_en_hist)
+    @inbounds for i in eachindex(mc_states)
+        index = find_hist_index(mc_states[i],results,delta_en_hist)
+        results.en_histogram[i][index] +=1
+    end
+    return results        
 end
 
 rdf_index(r2val,delta_r2) = floor(Int,(r2val/delta_r2))
@@ -85,13 +80,14 @@ function find_rdf_index(mc_state,delta_r2)
 
     return rdf_indices
 end
-"""
-    update_one_rdf!(mc_state,histogram,delta_r2)
-Function accepts one mc_state and the rdf histogram corresponding to it. It calculates the indices and broadcasts the update hist function defined for the energy histograms. This is a wrapper, becaues while results.rdf is a vector, results is not.
-"""
-function update_one_rdf!(mc_state,histogram,delta_r2)
-    rdf_indices = find_rdf_index(mc_state,delta_r2)
-    broadcast(update_one_histval!,Ref(histogram),rdf_indices)
+
+function update_one_rdf!(mc_state,results,delta_r2)
+    
+        rdf_indices = find_rdf_index(mc_state,delta_r2)
+        @inbounds for idx in rdf_indices 
+            results.rdf[i][idx] += 1
+        end
+    return results
 end
 """
     sampling_step!(mc_params,mc_states,save_index,results,delta_en_hist,delta_r2)
@@ -100,11 +96,11 @@ Function performed at the end of an mc_cycle! after equilibration. Updates the E
 """
 function sampling_step!(mc_params,mc_states,save_index,results,delta_en_hist,delta_r2)
     if rem(save_index, mc_params.mc_sample) == 0
-        mc_states =  update_energy_tot.(mc_states)#update energies
-
-        results = update_histograms!(mc_states,results,delta_en_hist) #update histograms
-
-        broadcast(update_one_rdf!,mc_states,results.rdf,Ref(delta_r2)) #update rdf's
+        for state in mc_states
+            state = update_energy_tot(state)
+            results = update_one_rdf!(state,results,delta_r2)
+        end
+        results = update_histograms!(mc_states,results,delta_en_hist)
     end
     return mc_states,results
 end
