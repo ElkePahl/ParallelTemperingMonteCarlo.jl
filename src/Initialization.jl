d856bd4a4c2aad38f05ec1f59267e28df0c0d059
grayseff <ghun245@aucklanduni.ac.nz>
2023-03-14 14:08:24 +0100

    working on method definitions

--- src/Initialization.jl
+++ src/Initialization.jl
@@ -99,7 +99,7 @@ function initialisation( pot, save_dir, restart::Bool;eq_percentage = 0.2,startf
     return mc_states,mc_params,move_strat,pot,ensemble,results,start_counter,n_steps,a,v,r
 
 end
-function initialisation(mc_states, move_strat, mc_params, pot, ensemble, results)
+function initialisation(mc_states, move_strat, mc_params, pot, ensemble, results;save=true,save_dir=pwd(),startfile="input.data")
 
     a,v,r = atom_move_frequency(move_strat),vol_move_frequency(move_strat),rot_move_frequency(move_strat)
     n_steps = a + v + r
