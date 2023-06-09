de47050510f832453452cbca67439aa5b5db3a72
Abi Murcott <amur906@aucklanduni.ac.nz>
2023-06-09 12:54:23 +1200

    On branch feature/configsample
     Changes to be committed:
    	modified:   CNA/readfile_and_classify.jl
    	modified:   configs/classify/10.199480584071857-classify.txt
    	modified:   configs/classify/10.589445417437284-classify.txt
    	modified:   configs/classify/10.994320085672069-classify.txt
    	modified:   configs/classify/11.41467464832213-classify.txt
    	modified:   configs/classify/11.851100960472277-classify.txt
    	modified:   configs/classify/12.304213506072362-classify.txt
    	modified:   configs/classify/12.774650263124611-classify.txt
    	modified:   configs/classify/13.263073601950383-classify.txt
    	modified:   configs/classify/13.77017121780105-classify.txt
    	modified:   configs/classify/14.296657099126138-classify.txt
    	modified:   configs/classify/14.843272532862045-classify.txt
    	modified:   configs/classify/15.410787148156732-classify.txt
    	modified:   configs/classify/16.0-classify.txt
    	modified:   configs/classify/5.0-classify.txt
    	modified:   configs/classify/5.1911689669640735-classify.txt
    	modified:   configs/classify/5.389647048714169-classify.txt
    	modified:   configs/classify/5.5957137004349-classify.txt
    	modified:   configs/classify/5.8096590619426705-classify.txt
    	modified:   configs/classify/6.0317843661996795-classify.txt
    	modified:   configs/classify/6.262402363446968-classify.txt
    	modified:   configs/classify/6.5018377615536735-classify.txt
    	modified:   configs/classify/6.750427683202518-classify.txt
    	modified:   configs/classify/7.00852214055522-classify.txt
    	modified:   configs/classify/7.276484528066175-classify.txt
    	modified:   configs/classify/7.55469213413827-classify.txt
    	modified:   configs/classify/7.8435366723412345-classify.txt
    	modified:   configs/classify/8.143424832940495-classify.txt
    	modified:   configs/classify/8.454778855513057-classify.txt
    	modified:   configs/classify/8.778037123456683-classify.txt
    	modified:   configs/classify/9.113654781229382-classify.txt
    	modified:   configs/classify/9.462104375188344-classify.txt
    	modified:   configs/classify/9.823876518930543-classify.txt

--- CNA/readfile_and_classify.jl
+++ CNA/readfile_and_classify.jl
@@ -16,15 +16,44 @@ using Graphs
 N_c=10   #Number of configurations
 N=13    #Number of atoms in each configuration
 cut=7.0
+sigma=2.782
 
 M=32
 tgrid = [5. *(16. /5.)^((i-1)/(M-1)) for i in 1:M]
+
+"
+Lennard-Jones dimer potential
+The /100000 is to prevent the energy gradient being too large.
+Otherwise the minimisation might push atoms apart at the first step.
+"
+function lj2(d)
+    if d>=0.9
+        sig6d6=sigma^6*d^(-3)
+        #e=sigma^12*d^(-6)-sigma^6*d^(-3)
+        e=sig6d6*(sig6d6-1)
+    else
+        e=0.50993431+7.9720358*(0.9-d)
+    end
+    return 4e/100000
+end
+
+"Lennard-Jones potential for the whole configuration"
+function lj(x)
+    E=0
+    for i=1:N-1
+        for j=i+1:N
+            E+=lj2((x[3*j-2]-x[3*i-2])^2+(x[3*j-1]-x[3*i-1])^2+(x[3*j]-x[3*i])^2)
+        end
+    end
+    return E
+end
 
 #println()
 filedir = "$dir/configs"
 savedir = "$filedir/classify"
 for i in eachindex(tgrid)
     temp = tgrid[i]
+    classifyfile = open("$savedir/$temp-classify.txt","w+")
     open("$filedir/$temp.xyz") do f
         for i=1:N_c
 	        configuration = Array{Float64}(undef,N,3) # Initialise 3D array for the atom coordinates for each configuration in the file.
@@ -39,17 +68,25 @@ for i in eachindex(tgrid)
 		    	    configuration[j,k] = parse(Float64,x) # Store the component
 		        end
             end
-            #println(configuration)
+            
             totalProfiles, atomicProfiles = CNA(configuration, N, cut, true, 1)
             #println(totalProfiles)
             #println(atomicProfiles)
-            open("$savedir/$temp-classify.txt","a") do io
-                println(io,cluster_classify(atomicProfiles,N))
-            end
+
+            x = configuration*2
+
+            class = cluster_classify(atomicProfiles,N)
+            
+            write(classifyfile, "$class \n")
+            od=OnceDifferentiable(lj,x; autodiff=:forward);     #gradient
+            result = Optim.minimizer(optimize(od, x, ConjugateGradient(),Optim.Options(g_tol=1e-8)))
+
+            #println(configuration)
         
             #println()
         end
     end
+    close(classifyfile)
 end
 
 end
