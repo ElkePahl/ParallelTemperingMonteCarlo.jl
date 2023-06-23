d5c012641f660861c506345bf807a9b3e7b8921b
Abigail Murcott <amur906@aucklanduni.ac.nz>
2023-06-23 14:36:43 +1200

    Changes to be committed: 	modified:   CNA/readfile_and_classify.jl 	modified:   configs/classify/10.199480584071857-classify.txt 	modified:   configs/classify/10.589445417437284-classify.txt 	modified:   configs/classify/10.994320085672069-classify.txt 	modified:   configs/classify/11.41467464832213-classify.txt 	modified:   configs/classify/11.851100960472277-classify.txt 	modified:   configs/classify/12.304213506072362-classify.txt 	modified:   configs/classify/12.774650263124611-classify.txt 	modified:   configs/classify/13.263073601950383-classify.txt 	modified:   configs/classify/13.77017121780105-classify.txt 	modified:   configs/classify/14.296657099126138-classify.txt 	modified:   configs/classify/14.843272532862045-classify.txt 	modified:   configs/classify/15.410787148156732-classify.txt 	modified:   configs/classify/16.0-classify.txt 	modified:   configs/classify/5.0-classify.txt 	modified:   configs/classify/5.1911689669640735-classify.txt 	modified:   configs/classify/5.389647048714169-classify.txt 	modified:   configs/classify/5.5957137004349-classify.txt 	modified:   configs/classify/5.8096590619426705-classify.txt 	modified:   configs/classify/6.0317843661996795-classify.txt 	modified:   configs/classify/6.262402363446968-classify.txt 	modified:   configs/classify/6.5018377615536735-classify.txt 	modified:   configs/classify/6.750427683202518-classify.txt 	modified:   configs/classify/7.00852214055522-classify.txt 	modified:   configs/classify/7.276484528066175-classify.txt 	modified:   configs/classify/7.55469213413827-classify.txt 	modified:   configs/classify/7.8435366723412345-classify.txt 	modified:   configs/classify/8.143424832940495-classify.txt 	modified:   configs/classify/8.454778855513057-classify.txt 	modified:   configs/classify/8.778037123456683-classify.txt 	modified:   configs/classify/9.113654781229382-classify.txt 	modified:   configs/classify/9.462104375188344-classify.txt 	modified:   configs/classify/9.823876518930543-classify.txt

--- CNA/readfile_and_classify.jl
+++ CNA/readfile_and_classify.jl
@@ -15,11 +15,15 @@ using Graphs
 
 N_c=10   #Number of configurations
 N=13    #Number of atoms in each configuration
-cut=7.0
+cut=7.5
 sigma=2.782
 
 M=32
 tgrid = [5. *(16. /5.)^((i-1)/(M-1)) for i in 1:M]
+
+ico = [5.294682606677612, 1.74009716698944, 0.9473289631755355, 3.791679084443151, -4.003030763557157, 1.2481962327125617, 3.81120559618501, -1.2068145890118294, -3.997172736460349, 1.8360724389860341, 4.332618355353545, -3.1331031921298242, 1.8044723389262654, -0.19175598302143088, 5.354081496111285, 0.5958439732022384, 4.959959644124769, 2.6462898025669164, -5.2946853312914826, -1.7400980998293896, -0.9473288849184892, -0.5958409506997832, -4.959961299680371, -2.6462896120220933, -1.836070528605722, -4.332620441173307, 3.133103314441934, -1.8044747891440631, 0.19175586145771703, -5.3540817785904125, -3.791676430898504, 4.003034841710014, -1.2481959566841079, -3.8112070445964297, 1.2068156228076719, 3.997172524612168, -9.63184319775816e-7, -3.161696747439155e-7, -1.7197856982327948e-7]
+
+
 
 "
 Lennard-Jones dimer potential
@@ -28,17 +32,19 @@ Otherwise the minimisation might push atoms apart at the first step.
 "
 function lj2(d)
     c=[-10.5097942564988, 989.725135614556, -101383.865938807, 3918846.12841668, -56234083.4334278, 288738837.441765]
-    if d>=0.9
+    #if d>=0.9
         e=0
         dummy = 6
-            for i in c
-            e += i * d^(dummy)
+        x = sqrt(d)
+        for i in c
+            e += i *  (1/x)^(dummy)
             dummy += 2
         end
-    else
-        e=0.50993431+7.9720358*(0.9-d)
-    end
-    return 4e/100000
+    #else
+    #    e=0.50993431+7.9720358*(0.9-d)
+    #end
+    #return 4e/100000
+    return e
 end
 
 "Lennard-Jones potential for the whole configuration"
@@ -56,8 +62,9 @@ end
 #println()
 filedir = "$dir/configs"
 savedir = "$filedir/classify"
-for i in eachindex(tgrid)
-    temp = tgrid[i]
+tempfile = open("$savedir/temp.txt", "w+")
+for t in eachindex(tgrid)
+    temp = tgrid[t]
     classifyfile = open("$savedir/$temp-classify.txt","w+")
     open("$filedir/$temp.xyz") do f
         for i=1:N_c
@@ -74,24 +81,40 @@ for i in eachindex(tgrid)
 		        end
             end
             
-            totalProfiles_i, atomicProfiles_i = CNA(configuration, N, cut, true, 1)
+            
+            
             #println(totalProfiles)
             #println(atomicProfiles)
+            #println(configuration)
+            x = zeros(3N)
+            for i = 1:N
+                x[3i-2] = configuration[i,1]
+                x[3i-1] = configuration[i,2]
+                x[3i] = configuration[i,3]
 
-            x = configuration*2
-
-            classi = cluster_classify(atomicProfiles_i,N)
+            end
+            #println(x)
+            
+            
             
             
             od=OnceDifferentiable(elj_ne,x; autodiff=:forward);     #gradient
             result = Optim.minimizer(optimize(od, x, ConjugateGradient(),Optim.Options(g_tol=1e-8)))
-
-            totalProfiles_f, atomicProfiles_f = CNA(result/2, N, cut, true, 1)
+            config_f = Array{Float64}(undef, N, 3)
+            for i = 1:N
+                config_f[i,1] = result[3i-2]
+                config_f[i,2] = result[3i-1]
+                config_f[i,3] = result[3i]
+            end
+            totalProfiles_i, atomicProfiles_i = CNA(configuration, N, cut, true, 1)
+            classi = cluster_classify(atomicProfiles_i,N)
+            totalProfiles_f, atomicProfiles_f = CNA(config_f, N, cut, true, 1)
             classf = cluster_classify(atomicProfiles_f,N)
 
-            #println(result/2)
-            
+            #println(elj_ne(result))
+
             write(classifyfile, "$classi => $classf \n")
+            
         end
     end
     close(classifyfile)
