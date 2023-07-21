e63a390b7576d650ec8e9d1ea1f5373d24b52c41
grayseff <ghun245@aucklanduni.ac.nz>
2023-07-21 13:27:45 +0200

    further type stability

--- src/MCStates.jl
+++ src/MCStates.jl
@@ -32,7 +32,7 @@ mutable struct MCState{T,N,BC}
     beta::T
     config::Config{N,BC,T}
     dist2_mat::Matrix{T}
-    en_atom_vec::Vector{T}
+    en_atom_vec::Array{T}
     en_tot::T
     ham::Vector{T}
     max_displ::Vector{T}
