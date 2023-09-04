72d42aaa7a5fc26cdb0b029582117b50bd59e0a8
DianaYu44 <83005930+DianaYu44@users.noreply.github.com>
2023-09-04 12:28:30 +1200

    Update MCStates.jl

--- src/MCStates.jl
+++ src/MCStates.jl
@@ -37,6 +37,7 @@ mutable struct MCState{T,N,BC}
     en_tot::T
     ham::Vector{T}
     max_displ::Vector{T}
+    max_boxlength::T
     count_atom::Vector{Int}
     count_vol::Vector{Int}
     count_rot::Vector{Int}
@@ -45,12 +46,12 @@ end
 
 function MCState(
     temp, beta, config::Config{N,BC,T}, dist2_mat, tan_mat, en_atom_vec, en_tot; 
-    max_displ = [0.1,0.1,1.,10.], count_atom = [0,0], count_vol = [0,0], count_rot = [0,0], count_exc = [0,0]
+    max_displ = [0.1,0.1,1.], max_boxlength = 10., count_atom = [0,0], count_vol = [0,0], count_rot = [0,0], count_exc = [0,0]
 ) where {T,N,BC}
     ham = T[]
     MCState{T,N,BC}(
         temp, beta, deepcopy(config), copy(dist2_mat), copy(tan_mat),copy(en_atom_vec), en_tot, 
-        ham, copy(max_displ), copy(count_atom), copy(count_vol), copy(count_rot), copy(count_exc)
+        ham, copy(max_displ), copy(max_boxlength), copy(count_atom), copy(count_vol), copy(count_rot), copy(count_exc)
         )
 end
 
