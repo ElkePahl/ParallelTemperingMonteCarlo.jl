d32a3d659f9308c10e90c580324c33d7e601b42c
grayseff <ghun245@aucklanduni.ac.nz>
2024-02-16 12:47:35 +1300

    fixed issues with tests

--- test/test_potentials.jl
+++ test/test_potentials.jl
@@ -8,10 +8,10 @@
     @test dimer_energy(pot1,2.) == dimer_energy(pot,2.)
     c=[-1.,2.,3.,-4.,5.]
     pot =  ELJPotential{5}(c)
-    @test dimer_energy(pot1,1.) == sum(c)
+    @test dimer_energy(pot,1.) == sum(c)
 
-    @test dimer_energy(pot1,1.) == sum(c)
-    @test dimer_energy(pot,2.) == dimer_energy(pot1,2.)
+    @test dimer_energy(pot1,1.) == sum(c1)
+
     v1 = SVector(1., 2., 3.)
     v2 = SVector(2.,4.,6.)
     v3 = SVector(0., 1., 0.)
