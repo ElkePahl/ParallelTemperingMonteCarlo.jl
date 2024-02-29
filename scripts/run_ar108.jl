50d69b58910fed6f824d0c2f26c3d4371533454e
Samuel-Case <srbcase@gmail.com>
2024-02-29 15:49:22 +1300

    ar scripts

---
+++ scripts/run_ar108.jl
@@ -0,0 +1,166 @@
+using ParallelTemperingMonteCarlo
+
+using Random
+
+
+#set random seed - for reproducibility
+Random.seed!(1234)
+
+# number of atoms
+n_atoms = 108
+pressure=101325*10000
+
+# temperature grid
+
+ti = 200
+tf = 400
+
+n_traj = 32
+
+temp = TempGrid{n_traj}(ti,tf) 
+
+# MC simulation details
+
+
+mc_cycles = 100 #default 20% equilibration cycles on top
+
+
+mc_sample = 1  #sample every mc_sample MC cycles
+
+#move_atom=AtomMove(n_atoms) #move strategy (here only atom moves, n_atoms per MC cycle)
+displ_atom = 1.0 # Angstrom
+max_vchange = 0.02
+n_adjust = 100
+
+max_displ_atom = [0.1*sqrt(displ_atom*temp.t_grid[i]) for i in 1:n_traj]
+
+mc_params = MCParams(mc_cycles, n_traj, n_atoms, mc_sample = mc_sample, n_adjust = n_adjust)
+
+#moves - allowed at present: atom, volume and rotation moves (volume,rotation not yet implemented)
+move_strat = MoveStrategy(atom_moves = n_atoms, vol_moves = 1)  
+#move_strat = MoveStrategy(atom_moves = n_atoms) 
+
+#ensemble
+
+ensemble = NPT(n_atoms,pressure*3.398928944382626e-14)
+
+#ensemble = NVT(n_atoms)
+
+#ELJpotential for neon
+#c1=[-10.5097942564988, 0., 989.725135614556, 0., -101383.865938807, 0., 3918846.12841668, 0., -56234083.4334278, 0., 288738837.441765]
+#elj_ne1 = ELJPotential{11}(c1)
+
+c=[-123.635101619510, 21262.8963716972, -3239750.64086661, 189367623.844691, -4304257347.72069, 35315085074.3605]
+pot = ELJPotentialEven{6}(c)
+
+#starting configurations
+r_start = 3.7782 #r_start is the desired min. radius between atoms in the starting config.
+L_start = 2*(r_start^2/2)^.5  #L_start refers to the distance between adjacent atoms which are parallel to the x or y axis
+Cell_Repeats = cbrt(n_atoms/4) #Find how many times the unit cell has been repeated in one direction.
+#isinteger(3.0)
+if isinteger(Cell_Repeats) == false error("number of atoms not correct for fcc") end
+
+#Generate fcc starting arrangement
+#1st generate a simple cubic atoms
+pos = Vector{Float64}[]
+for i in 0:(Cell_Repeats-1)
+  x = i * L_start
+  for j in 0:(Cell_Repeats-1)
+    y = j * L_start
+    for k in 0:(Cell_Repeats-1)
+      z = k * L_start
+      pos_entry = [x,y,z]
+      push!(pos, pos_entry)
+    end
+  end
+end 
+
+#2nd generate atoms that sit in the face of the unit cell, that sit in the same plane as simple cubic atoms
+for i in 0:(Cell_Repeats-1)
+  x = i * L_start + L_start/2
+  for j in 0:(Cell_Repeats-1)
+    y = j * L_start + L_start/2
+    for k in 0:(Cell_Repeats-1)
+      z = k * L_start
+      pos_entry = [x,y,z]
+      push!(pos, pos_entry)
+    end
+  end
+end 
+
+#3rd generate the rest of the atoms that sit in the face of the unit cell
+for i in 0:(Cell_Repeats-1)
+  x = i * L_start
+  for j in 0:(Cell_Repeats-1)
+    y = j * L_start + L_start/2
+    for k in 0:(Cell_Repeats-1)
+      z = k * L_start + L_start/2
+      pos_entry = [x,y,z]
+      push!(pos, pos_entry)
+    end
+  end
+end 
+
+for i in 0:(Cell_Repeats-1)
+  x = i * L_start + L_start/2
+  for j in 0:(Cell_Repeats-1)
+    y = j * L_start
+    for k in 0:(Cell_Repeats-1)
+      z = k * L_start  + L_start/2
+      pos_entry = [x,y,z]
+      push!(pos, pos_entry)
+    end
+  end
+end 
+
+#center starting configation at the orgin
+center = Vector{Float64}([Cell_Repeats * L_start/2, Cell_Repeats * L_start/2, Cell_Repeats * L_start/2])
+for l in 1:n_atoms
+  pos[l] = pos[l] - center
+end
+
+pos_ne32 = pos
+
+#convert to Bohr
+AtoBohr = 1.8897259886
+pos_ne32 = pos_ne32 * AtoBohr
+
+length(pos_ne32) == n_atoms || error("number of atoms and positions not the same - check starting config")
+
+#boundary conditions 
+box_length = Cell_Repeats * L_start * AtoBohr
+bc_ne32 = CubicBC(box_length)   
+
+#starting configuration
+start_config = Config(pos_ne32, bc_ne32)
+
+#histogram information
+n_bin = 100
+#en_min = -0.006    #might want to update after equilibration run if generated on the fly
+#en_max = -0.001    #otherwise will be determined after run as min/max of sampled energies (ham vector)
+
+#construct array of MCState (for each temperature)
+mc_states = [MCState(temp.t_grid[i], temp.beta_grid[i], start_config, pot; max_displ = [0.1,0.1,1.], max_boxlength = box_length*1.8) for i in 1:n_traj]
+
+println(mc_states[1].en_tot)
+
+println(mc_states[1].en_tot+ensemble.pressure*mc_states[1].config.bc.box_length^3)
+
+
+#results = Output(n_bin, max_displ_vec)
+results = Output{Float64}(n_bin; en_min = mc_states[1].en_tot)
+
+Random.seed!(1234)
+@time ptmc_run!((mc_states, move_strat, mc_params, pot, ensemble, results); save=false)
+
+
+plot(temp.t_grid,results.heat_cap)
+
+data = [sum(results.ev_histogram[i][:,j] for j=1:100) for i in 1:n_traj]
+
+#data = [results.en_histogram[i] for i in 1:n_traj]
+
+
+save_resultsNPT(results, "/Users/samuelcase/GIT/PTMC/ParallelTemperingMonteCarlo.jl/data/Ar108_1.0GPa_1000000")
+
+#plot(data)
