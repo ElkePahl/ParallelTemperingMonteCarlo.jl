60100aefb1be2987a5a3afade4c4fa56d89ca6e4
Diana Yu <tyu044@aucklanduni.ac.nz>
2024-03-15 11:08:14 +1300

    correction

--- test/runtests.jl
+++ test/runtests.jl
@@ -139,8 +139,9 @@ end
     @test abs(trial_config.bc.box_length/bc.box_length - trial_config.pos[1][1]/v1[1]) <= 10^(-15)
     @test abs(trial_config.bc.box_length/bc.box_length - trial_config.bc.box_height/bc.box_height) <= 10^(-15)
 
+    v5=[7.5, 4.330127018922193, 5.0]
     displ = 0.1
-    trial_pos = atom_displacement(v1,displ,bc)
+    trial_pos = atom_displacement(v5,displ,bc)
     @test norm(trial_pos-v1) < displ
 
 
