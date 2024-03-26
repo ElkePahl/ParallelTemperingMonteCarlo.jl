2dcba136ef44dfa14b959d69448199ddd7f622b4
grayseff <ghun245@aucklanduni.ac.nz>
2024-03-27 11:18:58 +1300

    new functions for read/save

--- src/Checkpoint.jl
+++ src/Checkpoint.jl
@@ -12,6 +12,9 @@ using ..Ensembles
 using ..EnergyEvaluation
 using ..MCStates
 
+
+export save_init,save_histparams,checkpoint
+export read_init,setresults,rebuild_states
 #---------------------------------------------------------------#
 #-----------------------Static Parameters-----------------------#
 #---------------------------------------------------------------#
@@ -37,8 +40,8 @@ function writeensemble(savefile,ensemble::NVT)
 end
 function writeensemble(savefile,ensemble::NPT)
 
-    headersvec = ["ensemble" "n_atom_moves" "n_volume_moves" "n_atom_swaps" ]
-    valuesvec = ["NPT{$(ensemble.n_atoms)}" ensemble.n_atom_moves ensembles.n_volume_moves emsemble.n_atom_swaps]
+    headersvec = ["ensemble" "n_atom_moves" "n_volume_moves" "n_atom_swaps" "pressure"]
+    valuesvec = ["NPT{$(ensemble.n_atoms)}" ensemble.n_atom_moves ensembles.n_volume_moves emsemble.n_atom_swaps ensemble.pressure]
     writedlm(savefile, [headersvec, valuesvec], ' ' )
 end
 """
@@ -49,7 +52,7 @@ Function to write potential surface information into `savefile`. implemented met
 function writepotential(savefile,potential::Ptype) where Ptype <: AbstractDimerPotential
     coeff_vec = transpose([potential.coeff[i] for i in eachindex(potential.coeff)])
 
-    write(savefile,"ELJ_coeffs: " )
+    write(savefile,"$(typeof(pot))" )
     writedlm(savefile, coeff_vec, ' ')
 end
 function writepotential(savefile,potential::Ptype) where Ptype <: AbstractDimerPotentialB
@@ -83,4 +86,246 @@ function save_init(potential,ensemble,params,temp)
         close(paramsfile)
     end
 end
+#-----------------------------------------------------------------#
+#-----------------------Post-Equilibration------------------------#
+#-----------------------------------------------------------------#
+"""
+    save_histparams(results)
+Initialises and populates a data file containing the information necessary to interpret histogram data
+"""
+function save_histparams(results)
+    resfile = open("./checkpoint/hist_info.data","w+")
+    headersvec = ["e_min" "e_max" "v_min" "v_max" "Δ_E" "Δ_V" "Δ_r2"]
+    infovec = [results.en_min results.en_max results.v_min results.v_max results.delta_en_hist results.delta_v_hist results.delta_r2]
+    writedlm(resfile,[headersvec , infovec], ' ')
+    close(resfile)
+end
+#-----------------------------------------------------------------#
+#-----------------------Save Configurations-----------------------#
+#-----------------------------------------------------------------#
+"""
+    checkpoint_config(savefile , config::Config{N,BC,T}, max_displ) 
+        BC=SphericalBC,CubicBC,RhombicBC
+Function writes a single config in the standard xyz format. `N` atoms, the comment line contains the boundary condition information (implemented for Spherical BC and bothtypes of Periodic BC) as well as `max_displ` information determining the stepsize used at the current step of the monte carlo simulation. The comment row is followed by 1 as a placeholder for the atom type to be implemented in future and the positions x,y,z in order.
+"""
+function checkpoint_config(savefile , config::Config{N,BC,T} , max_displ ) where {N,BC<:SphericalBC,T}
+    writedlm(savefile,[N,"$BC r2: $(config.bc.radius2) $(max_displ[1]) $(max_displ[2]) $(max_displ[3])"])
+    
+    for row in config.pos
+        write(savefile, "1  $(row[1]) $(row[2]) $(row[3]) \n")
+    end
+end
+function checkpoint_config(savefile , config::Config{N,BC,T} , max_displ ) where {N,BC<:CubicBC,T}
+    writedlm(savefile,[N,"$BC box_length: $(config.bc.radius2) $(max_displ[1]) $(max_displ[2]) $(max_displ[3])"])
+    
+    for row in config.pos
+        write(savefile, "1  $(row[1]) $(row[2]) $(row[3]) \n")
+    end
+end
+function checkpoint_config(savefile , config::Config{N,BC,T} , max_displ ) where {N,BC<:RhombicBC,T}
+    writedlm(savefile,[N,"$BC box_dims: $(config.bc.box_length) $(config.bc.box_height) $(max_displ[1]) $(max_displ[2]) $(max_displ[3])"])
+    
+    for row in config.pos
+        write(savefile, "1  $(row[1]) $(row[2]) $(row[3]) \n")
+    end
+end
+"""
+    save_checkpoint(mc_states::Vector{stype}) where stype <: MCState
+Function to save the configuration of each state in a vector of `mc_states`. writes each configuration according to [`writeconfig`](@ref) into a file config.i where i indicates the order of the states. 
+"""
+function save_configs(mc_states::Vector{stype}) where stype <: MCState
+    for saveindex in eachindex(mc_states)
+        checkpoint_file = open("./checkpoint.config.$saveindex","w")
+        checkpoint_config(checkpoint_file,mc_states[saveindex].config, mc_states[saveindex].max_displ)
+        close(checkpoint_file)
+    end
+end
+"""
+    checkpoint(index,mc_states,results,ensemble;rdfsave=false)
+Function to save relevant information about the current state of the system at step `index`. saves the configurations in each `mc_state` [`save_configs`](@ref) as well as the histograms stored in `results`. Optionally stores the volume histograms if using the NPT ensemble and the radial distribution functions if desired. 
+"""
+function checkpoint(index,mc_states,results,ensemble::NVT;rdfsave=false)
+    indexfile = open("./checkpoint/index.txt","w+")
+    writedlm(indexfile,index)
+    close(indexfile)
+    save_configs(mc_states)
+    histfile = open("./checkpoint/histograms.data","w+")
+    writedlm(histfile,results.en_histogram)
+    close(histfile)
+    if rdfsave == true 
+        rdffile = open("./checkpoint/rdf.data","w+")
+        writedlm(rdffile,results.rdf)
+    else
+    end
+    
+end
+function checkpoint(index,mc_states,results,ensemble::NPT; rdfsave=false)
+    indexfile = open("./checkpoint/index.txt","w+")
+    writedlm(indexfile,index)
+    close(indexfile)
+    save_configs(mc_states)
+    histfile = open("./checkpoint/histograms.data","w+")
+    writedlm(histfile,results.en_histogram)
+    close(histfile)
+    v_file = open("./checkpoint/volume_hist","w+")
+    writedlm(v_file,ev_histogram)
+    close(v_file)
+    if rdfsave == true 
+        rdffile = open("./checkpoint/rdf.data","w+")
+        writedlm(rdffile,results.rdf)
+    else
+    end    
+end
+#--------------------------------------------------------------------#
+#-----------------------------Read Files-----------------------------#
+#--------------------------------------------------------------------#
+"""
+    readensemble(ensemblevec)  
+Function to convert delimited file contents `ensemblevec` and convert them into an ensemble.
+
+"""
+function readensemble(ensemblevec)
+    if contains(ensemblevec[1],"NVT")
+        return NVT(ensemblevec[2],ensemblevec[3],ensemblevec[4])
+    elseif contains(ensemblevec[1],"NPT")
+        return NPT(ensemblevec[2],ensemblevec[3],ensemblevec[4],ensemblevec[5],ensemblevec[6])
+    end
+end
+"""
+    readpotential(potinfovec)
+Function to convert delimited file contents `potinfovec` into a potential. Implemented for:
+    ELJEven,ELJ,ELJB and EmbeddedAtomPotential
+"""
+function readpotential(potinfovec)
+
+    if potinfovec[1,1] == "ELJPotentialEven{nlength,Float64}"
+        coeffs= Vector(potinfovec[1,2:end])
+        return ELJPotentialEven(coeffs)
+    elseif potinfovec[1,1] == "ELJPotential{nlength,Float64}"
+        coeffs = Vector(potinfovec[1,2:end])
+        return ELJPotential(coeffs)
+    elseif potinfovec[1,1] == "EAM:"
+        return EmbeddedAtomPotential(potinfovec[1,2],potinfovec[1,3],potinfovec[1,4],potinfovec[1,5])
+    elseif potinfovec[1,1] == "ELJB"
+        a,b,c = potinfovec[2,:],potinfovec[3,:],potinfovec[4,:]
+        return ELJPotentialB(a,b,c)
+    end
+end
+"""
+    read_params(paramsvec)
+Function to turn a delimited `paramsvec` into an MCParams and TempGrid struct 
+"""
+function read_params(paramsvec)
+    parameters = MCParams(paramsvec[1],0,paramsvec[2],paramsvec[3],paramsvec[4],100,100)
+    temps = TempGrid{Int(paramsvec[3])}(paramsvec[5],paramsvec[6])
+    return parameters,temps
+end
+"""
+    read_init()
+Function to reinitialise the fixed parameters of the MC simulation as saved by the [`save_init`](@ref) function. returns 
+"""
+function read_init()
+    readfile=open("./checkpoint/params.data","r+")
+    data=readdlm(readfile)
+    close(readfile)
+
+    paramsvec=data[2,:]
+    ensemblevec=data[4,:]
+    potinfovec=data[5:end,:]
+
+    mc_params,temp = read_params(paramsvec)
+    ensemble = readensemble(ensemblevec)
+    potential=readpotential(potinfovec)
+
+    return mc_params,temp,ensemble,potential
+end
+
+"""
+    read_checkpoint_config(xyzdata)
+Function designed to take a single xyz-style checkpoint file and return the configuration and max displacement data associated with a saved mc_state. This is used to reconstruct an mc simulation from checkpoints
+"""
+function read_checkpoint_config(xyzdata)
+    N=xyzdata[1,1]
+    if confinfo[2,1] == "SphericalBC{Float64}"
+        bc = SphericalBC(;radius=sqrt(confinfo[2,3]))
+        
+        max_displ = [confinfo[2,4:6]]
+    elseif confinfo[2,1] == "CubicBC{Float64}"
+        bc = CubicBC(confinfo[2,3])
+
+        max_displ = [confinfo[2,4:6]]
+    elseif confinfo[2,1] == "RhombicBC"
+        bc = RhombicBC(confinfo[2,3],confinfo[2,4])
+        max_displ = [confinfo[2,5:7]]
+    end
+    configvectors = [xyzdata[2+i,2:4] for i in 1:N]
+
+    config=Config(configvectors,bc)
+
+    return config , max_displ
+end
+"""
+    setresults(histparams,histdata,histv_data,r2data)
+Function to re-initialise the results struct on restarting a simulation.
+"""
+function setresults(histparams,histdata,histv_data,r2data)
+    results=Output{Float64}(100)
+    results.en_min,results.en_max=histparams[1],histparams[2]
+    results.v_min,results.v_max = histparams[3],histparams[4]
+    results.delta_en_hist,results.delta_v_hist,results.delta_r2=histparams[5],histparams[6],histparams[7]
+
+    for row in eachrow(histdata)
+        push!(results.en_histogram,Vector(row))
+    end
+    if typeof(histv_data) == Matrix{Float64}
+        for row in eachrow(histv_data)
+            push!(results.ev_histogram,Vector(row))
+        end
+    end
+    if typeof(r2data) == Matrix{Float64}
+        for row in eachrow(r2data)
+            push!(results.rdf,Vector(row))
+        end
+    end
+    return results
+end
+"""
+    rebuild_states(n_traj,ensemble,temps,potential)
+Function to rebuild the MCStates vector and results struct from checkpoint information. The `ensemble` `temps` and `potential` along with `n_traj` are reconstructed elsewhere, but required to accurately recreate the states. 
+"""
+function rebuild_states(n_traj,ensemble,temps,potential)
+    histinfofile=open("./checkpoint/hist_info.data","r+")
+    histsfile=open("./checkpoint/histograms.data","r+")
+    dataread = readdlm(histinfofile)
+    histiread=readdlm(histsfile)
+    close(histinfofile)
+    close(histsfile)
+    if isfile("./checkpoint/rdf.data") == true
+        rdffile = open("./checkpoint/rdf.data","r+")
+        rdfinfo= readdlm(rdffile)
+        close(rdffile)
+    else
+        rdfinfo = false
+    end
+    if typeof(ensemble) == NPT
+        ev_file = open("./checkpoint/volume_hist.data","r+")
+        ev_info = readdlm(ev_file)
+        close(ev_file)
+    else
+        ev_info = false
+    end
+    results = setresults(dataread[2,:],histiread,ev_info,rdfinfo)
+    mcstates = []
+    for index in 1:n_traj
+        configfile = open("./checkpoint/config.$index","r+")
+        configinfo=readdlm(configfile)
+        close(configfile)
+        conf,maxdisp = read_checkpoint_config(configinfo)
+
+        state = MCState(temps.t_grid[index],temps.beta_grid[index],conf,ensemble,potential;max_displ=maxdisp[1])
+        push!(mcstates,state)
+    end
+    return mcstates,results
+end
+
 end
