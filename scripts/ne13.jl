5ae17548e7a28f5cdd7cce2f5187eab26ca49330
grayseff <ghun245@aucklanduni.ac.nz>
2024-04-02 10:38:26 +1300

    runscript now removes savefiles at the end

--- scripts/ne13.jl
+++ scripts/ne13.jl
@@ -91,5 +91,5 @@ start_config = Config(pos_ne13, bc_ne13)
 #to check code in REPL
 ptmc_run!(mc_params,temp,start_config,pot,ensemble;save=1000)
 
-#rm("checkpoint",recursive=true)
+rm("checkpoint",recursive=true)
 ## 
