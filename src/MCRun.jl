a88edfb24562123173c48c24494cf4bada13e970
DianaYu44 <83005930+DianaYu44@users.noreply.github.com>
2025-09-20 15:38:21 +1200

    changed the first print out to 1000 cycles, reduce time to debug

--- src/MCRun.jl
+++ src/MCRun.jl
@@ -133,7 +133,7 @@ function mc_cycle!(mc_states,move_strat,mc_params,pot,ensemble,n_steps,results,i
 #             save_results(results,save_dir)
 #         end
 #     end
-    if rem(idx,10000) == 0
+    if rem(idx,1000) == 0
         for i=1:length(mc_states)
             open("$(length(mc_states[1].config.pos))/configuration_$(mc_states[i].temp).txt","a") do io
                 println(io, length(mc_states[1].config.pos))
