5d242d517063dc59ca727cfde3e98ebd503d47e0
DianaYu44 <83005930+DianaYu44@users.noreply.github.com>
2025-09-20 20:47:01 +1200

    ..

--- src/MCRun.jl
+++ src/MCRun.jl
@@ -146,7 +146,10 @@ function mc_cycle!(mc_states,move_strat,mc_params,pot,ensemble,n_steps,results,i
     println("cycle: ",idx)
     println("mc_states[1].en_tot: ",mc_states[1].en_tot)
     println("mc_states[1].new_en: ",mc_states[1].new_en)
-    println("dimer_energy_config: ",dimer_energy_config(mc_states[1].dist2_mat, 216, mc_states[1].potential_variables, mc_states[1].ensemble_variables.r_cut, mc_states[1].config.bc, potential)[2])
+    mc_state.potential_variables.update==true
+    println("dimer_energy_config true: ",dimer_energy_config(mc_states[1].dist2_mat, 216, mc_states[1].potential_variables, mc_states[1].ensemble_variables.r_cut, mc_states[1].config.bc, potential)[2])
+    mc_state.potential_variables.update==false
+    println("dimer_energy_config false: ",dimer_energy_config(mc_states[1].dist2_mat, 216, mc_states[1].potential_variables, mc_states[1].ensemble_variables.r_cut, mc_states[1].config.bc, potential)[2])
 
     if rem(idx,1000) == 0
         for i=1:length(mc_states)
