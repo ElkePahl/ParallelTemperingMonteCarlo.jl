6d15d5893553db2492a09c150bb3c8eb76e50ed1
DianaYu44 <83005930+DianaYu44@users.noreply.github.com>
2025-09-21 02:59:29 +1200

    e-9 to 10^(-9)

--- src/EnergyEvaluation.jl
+++ src/EnergyEvaluation.jl
@@ -522,7 +522,7 @@ function dimer_energy_config(distmat, NAtoms,potential_variables::LookupTableVar
     dimer_energy_vec = zeros(NAtoms)
     energy_tot = 0.
 
-    if abs(potential_variables.new_tan_mat[1,2]-potential_variables.tan_mat[1,2])>=e-9
+    if abs(potential_variables.new_tan_mat[1,2]-potential_variables.tan_mat[1,2])>=10^(-9)
         println("new_tan_mat!=tan_mat, use new_tan_mat")
         println("new: ",potential_variables.new_tan_mat[1,2])
         println("old: ",potential_variables.tan_mat[1,2])
