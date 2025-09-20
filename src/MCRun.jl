23b4522a89ad1b5538e3be5984041f454f2db6b9
DianaYu44 <83005930+DianaYu44@users.noreply.github.com>
2025-09-20 15:46:58 +1200

    changed the print command

--- src/MCRun.jl
+++ src/MCRun.jl
@@ -138,7 +138,7 @@ function mc_cycle!(mc_states,move_strat,mc_params,pot,ensemble,n_steps,results,i
             open("$(length(mc_states[1].config.pos))/configuration_$(mc_states[i].temp).txt","a") do io
                 println(io, length(mc_states[1].config.pos))
                 println(io,mc_states[i].en_tot)
-                println(dimer_energy_config(mc_states[i].dist2_mat, 216, mc_states[i].potential_variables, mc_states[i].ensemble_variables.r_cut, mc_states[i].config.bc, potential)[2])
+                println(io,dimer_energy_config(mc_states[i].dist2_mat, 216, mc_states[i].potential_variables, mc_states[i].ensemble_variables.r_cut, mc_states[i].config.bc, potential)[2])
                 println(io,mc_states[i].config.bc.box_length)
                 println(io,mc_states[i].config.bc.box_height)
                 for j=1:length(mc_states[1].config.pos)
