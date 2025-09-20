eff9e48c31f0ddcc571f3fdf1fdbdf267aedbf3c
DianaYu44 <83005930+DianaYu44@users.noreply.github.com>
2025-09-20 18:39:09 +1200

    try copy new tan mat to tan mat

--- src/swap_config.jl
+++ src/swap_config.jl
@@ -139,7 +139,7 @@ function swap_config_v!(mc_state::MCState,potential_variables::LookupTableVariab
     if mc_state.potential_variables.tan_mat[1,2]!=mc_state.potential_variables.new_tan_mat[1,2]
         #println("swap")
         for i in eachindex(mc_state.potential_variables.tan_mat)
-            mc_state.potential_variables.tan_mat[i] = mc_state.potential_variables.new_tan_mat[i]
+            mc_state.potential_variables.tan_mat[i] = copy(mc_state.potential_variables.new_tan_mat[i])
         end
     end
     mc_state.en_tot = new_en_tot
