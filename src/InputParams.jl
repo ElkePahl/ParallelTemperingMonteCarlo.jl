aff7f65d4f48803cfd106d08f2d4b3f2fa99bf2b
DianaYu44 <83005930+DianaYu44@users.noreply.github.com>
2026-07-03 17:04:24 +1200

    Update InputParams.jl

--- src/InputParams.jl
+++ src/InputParams.jl
@@ -13,7 +13,7 @@ using ..EnergyEvaluation
 using ..CustomTypes
 
 export InputParameters
-export MCParams, TempGrid
+export MCParams, TempGrid, TempGrid_customised
 export Output
 
 const kB = 3.16681196E-6  # in Hartree/K (3.166811429E-6)
@@ -93,6 +93,12 @@ function TempGrid{N}(ti::Number, tf::Number; tdistr=:geometric) where {N}
 end
 
 TempGrid(ti::Number, tf::Number, N::Int; tdistr=:geometric) = TempGrid{N}(ti, tf; tdistr)
+
+function TempGrid_customised(tgrid::Array{Float64})
+    N=length(tgrid)
+    betagrid = 1.0 ./ (kB .* tgrid)
+    return TempGrid{N,eltype(tgrid)}(SVector{N}(tgrid), SVector{N}(betagrid))
+end
 
 """
     Output{T}(n_bin; en_min = 0) where T <: Number
