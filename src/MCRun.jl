41bf3ab56d2c2ff04a398dba40d0c7bf7a5af4c7
DianaYu44 <83005930+DianaYu44@users.noreply.github.com>
2026-07-07 14:19:19 +1200

    Update MCRun.jl
    Added the print config lines in mc cycle

--- src/MCRun.jl
+++ src/MCRun.jl
@@ -204,6 +204,24 @@ function mc_cycle!(
     rdfsave::Bool,
     potential,
 ) where {N,E}
+
+    if rem(idx,10000) == 0
+        for i=1:length(mc_states)
+            open("$(length(mc_states[1].config))/configuration_$(mc_states[i].temp).txt","a") do io
+                println(io, length(mc_states[1].config))
+                #println(io,mc_states[i].en_tot)
+                #println(io,mc_states[i].new_en)
+                #println(io,dimer_energy_config(mc_states[i].dist2_mat, 216, mc_states[i].potential_variables, mc_states[i].ensemble_variables.r_cut, mc_states[i].config.boundary_condition, potential)[2])
+                println(io,mc_states[i].config.boundary_condition.box_length)
+                println(io,mc_states[i].config.boundary_condition.box_height)
+                for j=1:length(mc_states[1].config)
+                    println(io, "Ar ", mc_states[i].config[j][1]," ",mc_states[i].config[j][2]," ",mc_states[i].config[j][3])
+                end
+                println(io," ")
+            end
+        end
+    end
+
     mc_states = mc_cycle!(mc_states, move_strat, mc_params, pot, ensemble, n_steps, idx)
 
     if rem(idx, mc_params.mc_sample) == 0
