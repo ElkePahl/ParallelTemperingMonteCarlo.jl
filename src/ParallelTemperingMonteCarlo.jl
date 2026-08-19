1d35cfac2fd4cd5f396e48e8bfe786e037d5a009
mtsch <matijacufar@gmail.com>
2026-08-20 11:38:38 +1200

    initial implementation

--- src/ParallelTemperingMonteCarlo.jl
+++ src/ParallelTemperingMonteCarlo.jl
@@ -22,6 +22,7 @@ include("MCRun.jl")
 include("multihist.jl")
 include("multihist_NPT.jl")
 include("multihist_NVT.jl")
+include("multihistogram_analysis.jl")
 
 include("configuration_presets.jl")
 
